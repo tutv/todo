f849b17588e49f939fdd554949cb29be43bb97e4
Tu TV <tutv95@gmail.com>
2015-08-12 00:26:43 +0700

    Almost done interface

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+    require('jit-grunt')(grunt);
+
+    grunt.initConfig({
+        less: {
+            development: {
+                options: {
+                    compress: true,
+                    yuicompress: true,
+                    optimization: 2
+                },
+                files: {
+                    "assets/css/style.css": "assets/less/style.less" // destination file and source file
+                }
+            }
+        },
+        watch: {
+            styles: {
+                files: ['assets/less/**/*.less'], // which files to watch
+                tasks: ['less'],
+                options: {
+                    livereload: true
+                }
+            }
+        }
+    });
+
+    grunt.event.on('watch', function (action, filepath, target) {
+        grunt.log.writeln(target + ': ' + filepath + ' has ' + action)
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['less', 'watch']);
+};
